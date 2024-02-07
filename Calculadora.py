8daefc8fd50ff48d405e5e94103569f1a5f1ffeb
emmanuelcalad0615 <emmanuelcalad@gmail.com>
2024-02-07 13:32:01 -0500

    Add div and mutl

--- Calculadora.py
+++ Calculadora.py
@@ -2,4 +2,11 @@ def add(num1, num2):
     return num1 + num2
 
 def rest(num1, num2):
-    return num1-num2
+    return num1 - num2
+
+def mult(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+
